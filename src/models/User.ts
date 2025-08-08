84021fdb51a4c68c4031e787b2cace056f01460c
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-08-08 23:22:02 +0530

    fixed user error

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
-    password: String
-}, { timestamps: true });
+    password: { type: String, required: true },
+})
 
 export default mongoose.model("User", userSchema);
