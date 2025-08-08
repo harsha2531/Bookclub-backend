804e58246a4d23041c4deed4da41af8cad642cac
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-08-08 18:36:00 +0530

    added reader routes

---
+++ src/routes/ReaderRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import Reader from "../models/Reader";
+import auth from "../middleware/Auth";
+
+const router = Router();
+
+router.get("/", auth, async (_, res) => {
+    res.json(await Reader.find());
+});
+
+router.post("/", auth, async (req, res) => {
+    res.json(await Reader.create(req.body));
+});
+
+router.put("/:id", auth, async (req, res) => {
+    res.json(await Reader.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+router.delete("/:id", auth, async (req, res) => {
+    await Reader.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted" });
+});
+
+export default router;
