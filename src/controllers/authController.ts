3c40f2ac814212d8acef2c1e46b8b624f6dab233
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-08-08 20:29:54 +0530

    added auth controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+        expiresIn: "1d",
+    });
+
+    res.json({ token });
+};
+
+export const register = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword });
+    res.json(newUser);
+};
