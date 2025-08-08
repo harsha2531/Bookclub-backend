6cd461b71ce2e527a2a5ce1e0f5e23f0f511ea54
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-08-08 18:52:31 +0530

    changed index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
+
+// Routes
+import authRoutes from "./routes/authRoutes";
 import readerRoutes from "./routes/readerRoutes";
+import bookRoutes from "./routes/bookRoutes";
+import lendingRoutes from "./routes/lendingRoutes";
+import overdueRoutes from "./routes/overdueRoutes";
 
 dotenv.config();
 connectDB();
@@ -11,7 +17,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/readers", readerRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/lending", lendingRoutes);
+app.use("/api/overdue", overdueRoutes);
 
 app.get("/", (_, res) => res.send("BookClub API Running"));
 
